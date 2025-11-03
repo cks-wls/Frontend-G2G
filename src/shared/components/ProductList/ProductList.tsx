62916686727465fe627967b2a44ce6204d25b55e
seulbi-dev <dltmfql456@naver.com>
2025-11-04 02:54:17 +0900

    refactor: 페이지 이동을 위한 nvaigate 함수 대신 Link 컴포넌트 사용 (#17)
    - 초기화 스타일 추가
    - 스타일 및 클래스명 수정
    - 시맨틱 태그로 변경

--- src/shared/components/ProductList/ProductList.tsx
+++ src/shared/components/ProductList/ProductList.tsx
@@ -1,20 +1,18 @@
 import Card from '@/shared/components/Card/Card'
-import './ProductList.scss'
 import type { Product } from '@/types/product'
+import './ProductList.scss'
 
 interface ProductListProps {
   data: Product[]
 }
 
-const ProductList = ({data}: ProductListProps) => {
+const ProductList = ({ data }: ProductListProps) => {
   return (
-    <div>
-      <div className='container'>
-        {data.map((product) => 
-          <Card key={product.id} product={product} />
-        )}
-      </div>
-    </div>
+    <ul className="product-list">
+      {data.map((product) => (
+        <Card key={product.id} product={product} />
+      ))}
+    </ul>
   )
 }
 
