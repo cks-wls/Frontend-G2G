a64d7a5994360d93bc579be062860b5da76dcef3
cks-wls <slo0521@naver.com>
2025-11-03 15:27:45 +0900

    refactor : kebab-case에 맞게 네이밍 수정

--- src/shared/components/Form/Producer/ProducerNumberForm.tsx
+++ src/shared/components/Form/Producer/ProducerNumberForm.tsx
@@ -1,7 +1,7 @@
 import "@/shared/components/Form/Producer/ProducerNumberFormStyle.scss"
 
 function ProducerNumberForm(){
-    return <input className = "producerNumberForm" placeholder = "- 를 제외한 사업자 번호 10자리를 입력해주세요"></input>
+    return <input className = "producer-number-form" placeholder = "- 를 제외한 사업자 번호 10자리를 입력해주세요"></input>
     // 이후 API와 연동시켜야함
 }
 
--- src/shared/components/Form/Search/Search.tsx
+++ src/shared/components/Form/Search/Search.tsx
@@ -2,9 +2,9 @@ import searchIcon from "@/assets/icons/searchIcon.svg";
 import "@/shared/components/Form/Search/SearchStyle.scss"
 function Search(){
     return (
-    <div className = "searchBox">
+    <div className = "search-box">
     <input className = "input" placeholder = "원하는 상품을 검색하세요"></input>
-    <img className = "searchIcon" src = {searchIcon} />
+    <img className = "search-icon" src = {searchIcon} />
     </div>
     // 추후 검색 API 연동이 되면 API와 연동시키기
     )
