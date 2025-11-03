5d1528eaa0b6dc910fd924a34e8ed19f540c1aa8
cks-wls <slo0521@naver.com>
2025-11-03 19:22:23 +0900

    chore : 네이밍 규칙에 맞게 경로 수정

--- src/shared/components/Form/Option/OptionModal.tsx
+++ src/shared/components/Form/Option/OptionModal.tsx
@@ -1,6 +1,6 @@
 import '@/shared/components/Form/Option/OptionModalStyle.scss'
 import bottomArrow from '@/assets/icons/bottomArrow.svg'
-import optionConstants from '@/constants/optionConstants'
+import optionConstants from '@/constants/OPTION_CONSTANTS'
 import { useState } from 'react'
 
 export type ModalSize = 'md' | 'lg'
