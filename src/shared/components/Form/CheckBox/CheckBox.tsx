8dfe90ae0d37c610736f263b4eb58e574e0bd65f
cks-wls <slo0521@naver.com>
2025-11-05 14:22:06 +0900

    feat : 체크박스 요구사항에 맞게 구현(#16)

--- src/shared/components/Form/CheckBox/CheckBox.tsx
+++ src/shared/components/Form/CheckBox/CheckBox.tsx
@@ -1,43 +1,57 @@
+import checkBoxConstants from '@/constants/CHECKBOX_CONSTANTS'
 import '@/shared/components/Form/CheckBox/CheckBoxStyle.scss'
 import { useState } from 'react'
 
 function CheckBox() {
-  const [currentChecked, setCurrentChecked] = useState('')
-  const handleCheck = (val: string) => {
-    setCurrentChecked((prev) => (prev === val ? '' : val))
+  const totalCount = checkBoxConstants.length // 추후에 API와 연동해서 수정할 예정
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]) // 선택된 옵션을 배열로 관리
+  const handleOptionCheck = (option: string) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((val) => val !== option))
+    } else {
+      setSelectedOptions([...selectedOptions, option])
+    }
   }
-  //  체크박스를 하나만 선택할 수 있도록 설정(이미 선택된 값이면 해제, 아니면 선택)
+  // 개별 옵션 선택 핸들러
+  const handleSelectAll = () => {
+    if (selectedOptions.length === totalCount) {
+      setSelectedOptions([])
+    } else {
+      setSelectedOptions([...checkBoxConstants])
+    }
+  } // 전체 선택 / 해제 핸들러
+
   return (
     <>
-      <label>
-        {/* 이곳에 텍스트 입력 */}
-        <input
-          type="checkbox"
-          value="1"
-          className="check-box"
-          checked={currentChecked === '1'} // 체크되었을때 상태변화
-          onChange={() => handleCheck('1')} // 체크박스 클릭시 handleCheck 함수 실행
-        />
-        {/* 이곳에 텍스트 입력 */}
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          value="2"
-          className="check-box"
-          checked={currentChecked === '2'}
-          onChange={() => handleCheck('2')}
-        />
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          value="3"
-          className="check-box"
-          checked={currentChecked === '3'}
-          onChange={() => handleCheck('3')}
-        />
-      </label>
+      <div className="all-select-box">
+        <label htmlFor="totalCheck">
+          <input
+            type="checkbox"
+            value="all"
+            className="check-box"
+            id="totalCheck"
+            onClick={handleSelectAll}
+            checked={selectedOptions.length === totalCount}
+          />
+          전체 선택 ({selectedOptions.length}/{totalCount})
+          {/* 이후에 API와 연동해 전체 개수 연동하기 */}
+        </label>
+      </div>
+      {checkBoxConstants.map((option, index) => (
+        <div className="option-box" key={index}>
+          <label htmlFor={`check-${index}`}>
+            <input
+              type="checkbox"
+              value={option}
+              className="check-box"
+              id={`check-${index}`}
+              checked={selectedOptions.includes(option)}
+              onChange={() => handleOptionCheck(option)}
+            />
+            {option}
+          </label>
+        </div>
+      ))}
     </>
   )
 }
