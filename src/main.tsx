8693263caceb8483a6149c2e0a9da799ce6f2e85
seulbi-dev <dltmfql456@naver.com>
2025-11-05 17:54:30 +0900

    chore: setup MSW (#32)

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,26 @@ import { createRoot } from 'react-dom/client'
 import '@/foundations/index.scss'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-)
+
+// MSW 워커를 활성화하는 함수
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development') {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser')
+
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+})
