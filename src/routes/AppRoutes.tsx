d669a31f811f8da4f36c0dded26011374872c0a6
seulbi-dev <dltmfql456@naver.com>
2025-11-06 16:39:39 +0900

    refactor: Header Footer 컴포넌트로 교체 및 라우트 설정 (#30)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,29 @@
-import Main from "@/pages/Main";
-import Layout from "@/shared/components/Layout";
-import { Route, Routes } from "react-router-dom";
+import { ROUTE_PATHS } from '@/constants/route'
+import MainPage from '@/pages/MainPage'
+import CommonLayout from '@/shared/components/Layout/CommonLayout'
+import MypageLayout from '@/shared/components/Layout/MypageLayout'
+import SellerLayout from '@/shared/components/Layout/SellerLayout'
+import { Route, Routes } from 'react-router-dom'
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Main />} />
+      {/* 게스트와 소비자 공용 레이아웃 */}
+      <Route element={<CommonLayout />}>
+        <Route index element={<MainPage />} />
+
+        {/* 소비자만 접근 가능 */}
+        <Route path={ROUTE_PATHS.MYPAGE.INDEX} element={<MypageLayout />}>
+        <Route index element={<MainPage />} />
+        
+        </Route>
+      </Route>
+
+      <Route path={ROUTE_PATHS.SELLER.INDEX} element={<SellerLayout />}>
+      <Route index element={<MainPage />} />
+        
       </Route>
     </Routes>
-  );
+  )
 }
-export default AppRoutes;
+export default AppRoutes
---
+++ src/shared/components/Layout/MypageLayout.tsx
@@ -0,0 +1,18 @@
+import SideNavigation from '@/shared/components/SideNavigation'
+import { Outlet } from 'react-router-dom'
+import styles from './Layout.module.scss'
+
+const MypageLayout = () => {
+  return (
+    <div className={styles['consumer-bg']}>
+      <div className={styles['consumer-main']}>
+        <SideNavigation userName="" />
+        <div className={styles.main}>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MypageLayout
