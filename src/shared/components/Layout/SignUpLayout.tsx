46d28e686d2c232e8481ad074b247a1d8a1d30e0
cks-wls <slo0521@naver.com>
2025-11-07 16:23:57 +0900

    chore : 용도에 맞게 파일 이름 수정(#39)

--- src/shared/components/Layout/SignUpLayout.tsx
+++ src/shared/components/Layout/SignUpLayout.tsx
@@ -2,7 +2,7 @@ import Header from '@/shared/components/Header'
 import { Outlet } from 'react-router-dom'
 import styles from './Layout.module.scss'
 
-const LoginLayout = () => {
+const SignUpLayout = () => {
   return (
     <div className={styles.container}>
       <Header />
@@ -13,4 +13,4 @@ const LoginLayout = () => {
   )
 }
 
-export default LoginLayout
+export default SignUpLayout
