8ddb607b56f8cab456976396c805a3929c076657
cks-wls <slo0521@naver.com>
2025-11-07 16:19:26 +0900

    feat : 공통 컴포넌트에 맞게 기능 수정(#39)

--- src/shared/components/Form/Producer/ProducerNumberForm.tsx
+++ src/shared/components/Form/Producer/ProducerNumberForm.tsx
@@ -2,21 +2,32 @@ import '@/shared/components/Form/Producer/ProducerNumberFormStyle.scss'
 interface ProducerNumberFormProps {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
   isError?: boolean
+  isDisabled?: boolean
   placeHolder?: string
-  type?: 'text' | 'email' | 'password' | 'number'
+  type?: 'text' | 'email' | 'password'
+  name?: string
+  readOnly?: boolean
+  value?: string
 }
 function ProducerNumberForm({
   onChange,
   isError,
+  isDisabled,
   placeHolder = '사업자등록번호 입력(-없이 숫자만 입력)',
   type = 'text',
+  name,
+  readOnly,
+  value,
 }: ProducerNumberFormProps) {
   return (
     <input
-      className={`producer-number-form ${isError ? 'error-form' : ''}`}
+      className={`producer-number-form ${isError ? 'error-form' : ''} ${isDisabled ? 'disabled' : ''}`}
       placeholder={placeHolder}
       onChange={onChange}
       type={type}
+      name={name}
+      readOnly={readOnly}
+      value={value}
     ></input>
   )
   // 이후 API와 연동시켜야함
