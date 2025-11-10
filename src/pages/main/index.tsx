264e29797cc8bbf1adc2de89fbf925c5f9f7c1e2
seulbi-dev <dltmfql456@naver.com>
2025-11-11 00:22:02 +0900

    fix: 사용하지 않는 컴포넌트 제거 (#48)

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,10 @@
 import SlideBanner from '@/components/main/SlideBanner'
 import styles from './MainPage.module.scss'
-import ProductCarousel from '@/components/main/ProductCarousel'
 
 const MainPage = () => {
   return (
     <div className={styles.wrap}>
       <SlideBanner />
-      <ProductCarousel />
     </div>
   )
 }
