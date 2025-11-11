3af0efef47570b4c02451bf13dd84caecd0853b5
seulbi-dev <dltmfql456@naver.com>
2025-11-11 17:57:30 +0900

    fix: 상품 타입 정의 (#53)

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,15 +1,37 @@
+// TODO: 엡체명, 할인율 추가 필요
 export interface Product {
-  id: number
+  product_id: number
+  categories: number[]
+  images: ProductImage[]
+  option_values: ProductOptionValue[]
   name: string
-  business_name: string
-  price: number
-  discount_rate?: number
-  final_price?: number
-  img_url?: string
-  description?: string
-  order_count?: number
-  like_count?: number
-  review_count?: number
-  created_at?: string
-  updated_at?: string
+  origin: string
+  stock: number
+  price: string
+  overseas_shipping: boolean
+  delivery_fee: string
+  description: string
+  sold_out: boolean
+  created_at: string
+  updated_at: string
+  seller: number
+}
+
+export interface ProductImage {
+  image_id: number
+  product: number
+  user: number
+  image_url: string
+}
+
+export interface ProductOptionValue {
+  id: number
+  category: ProductOptionCategory
+  extra_price: string
+}
+
+export interface ProductOptionCategory {
+  id: number
+  group_name: string
+  name: string
 }
