f07eab39f39d8db6439e598f0530bf6d9d320ee3
cks-wls <slo0521@naver.com>
2025-11-12 11:50:10 +0900

    feat : 토큰 관련 로직 추가(#51)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,16 @@
 import { API_BASE_URL } from '@/constants/api'
 import axios from 'axios'
+import { getAccessToken } from './auth'
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = getAccessToken()
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
 })
